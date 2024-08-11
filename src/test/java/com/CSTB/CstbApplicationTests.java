de042b012ceea27ff61e5a3696510f0fafa92727
Vinicius Hessel <42287069+Hesselzitos@users.noreply.github.com>
2024-08-11 13:27:51 -0300

    feat: first hello from Publisher!

---
+++ src/test/java/com/CSTB/CstbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSTB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CstbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
